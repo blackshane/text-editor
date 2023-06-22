443cfe9a1df00a618f58153d8209c3d6e5670d13
blackshane <122633142+blackshane@users.noreply.github.com>
2023-06-21 23:32:14 -0500

    Updated error handling w/ ty catch and logo render correctly

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,40 +1,47 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+const initdb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    if (db.objectStoreNames.contains('jate')) {
+      console.log('jate database already exists');
+      return;
+    }
+    const objectStore = db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+    console.log('jate database created', objectStore);
+  } catch (error) {
+    console.error('Error initializing the database:', error);
+  }
+};
 
 // Accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.add({ jate: content });
-  const result = await request;
-  console.log('Data saved', result)
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ jate: content });
+    const result = await request;
+    console.log('Data saved:', result);
+  } catch (error) {
+    console.error('Error while putting data into the database:', error);
+  }
 };
-
 
 // Gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.getAll();
-
-  const result = await request;
-  console.log('result.value', result);
-  return result;
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('Fetched data:', result);
+    return result;
+  } catch (error) {
+    console.error('Error while fetching data from the database:', error);
+    return [];
+  }
 };
 
 initdb();
